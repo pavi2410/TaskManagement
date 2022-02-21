a84b7052519e9d41d8cc63452cbe88850ad06202
Pavitra Golchha <hello@pavi2410.me>
2022-02-21 12:26:42 +0530

    Mantine

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,5 +1,6 @@
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
+import { injectStylesIntoStaticMarkup } from "@mantine/ssr";
 import  "dotenv/config"
 
 export default function handleRequest(
@@ -14,7 +15,7 @@ export default function handleRequest(
 
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response("<!DOCTYPE html>" + injectStylesIntoStaticMarkup(markup), {
     status: responseStatusCode,
     headers: responseHeaders
   });
