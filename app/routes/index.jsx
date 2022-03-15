d9855aaa8b616a69a7f22ef85195ef86e9097e70
Pavitra Golchha <hello@pavi2410.me>
2022-03-15 22:00:32 +0530

    home page

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,92 @@
-import { Button } from '@mantine/core';
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Button,
+  Group
+} from '@mantine/core';
+import { Link } from 'remix';
+import image from '../assets/undraw_taking_notes.svg';
 
-export default function Index() {
-  return <Button>Hello world!</Button>;
+const useStyles = createStyles((theme) => ({
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: theme.spacing.xl * 4,
+    paddingBottom: theme.spacing.xl * 4,
+  },
+
+  content: {
+    maxWidth: 480,
+    marginRight: theme.spacing.xl * 3,
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+      marginRight: 0,
+    },
+  },
+
+  title: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 44,
+    lineHeight: 1.5,
+    fontWeight: 900,
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 28,
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('xs')]: {
+      flex: 1,
+    },
+  },
+
+  image: {
+    flex: 1,
+
+    [theme.fn.smallerThan('md')]: {
+      display: 'none',
+    },
+  },
+
+  highlight: {
+    position: 'relative',
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.fn.rgba(theme.colors[theme.primaryColor][6], 0.55)
+        : theme.colors[theme.primaryColor][0],
+    borderRadius: theme.radius.sm,
+    padding: '4px 12px',
+  },
+}));
+
+export default function () {
+  const { classes } = useStyles();
+  return (
+    <div>
+      <Container>
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              A User-friendly <br /> <span className={classes.highlight}>Task Managment</span> <br /> app
+            </Title>
+
+            <Group mt={30}>
+              <Button radius="xl" size="md" className={classes.control} component={Link} to="/tasks">
+                Get started
+              </Button>
+              <Button variant="default" radius="xl" size="md" className={classes.control} component="a" href="https://github.com/pavi2410/TaskManagement">
+                Source code
+              </Button>
+            </Group>
+          </div>
+          <Image src={image} className={classes.image} />
+        </div>
+      </Container>
+    </div>
+  );
 }
